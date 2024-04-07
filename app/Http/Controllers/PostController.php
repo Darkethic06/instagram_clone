41c0293908452e086240b4d8e0c101547a570246
Darkethic06 <arijitpaul062@gmail.com>
2024-04-07 17:12:08 +0530

    change

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,41 +16,9 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
     {
         //
     }
